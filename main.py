ca4ec2af71ea82ecd87ad6b611124bd0adba6c04
YashSakhareliya <sakhareliyayash@gmail.com>
2024-05-15 21:27:07 +0530

    adding title, images and source link

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images\Yash_Photo.jpg")
+    st.image("images\Yash_Photo.jpg", width=400)
 
 
 with col2:
@@ -14,4 +15,28 @@ with col2:
     
     """
     st.info(context)
+
+
+context2 = """
+This is project description 
+"""
+st.write(context2)
+
+df = pd.read_csv("data.csv",sep=";")
+col3, empty_col,col4 = st.columns([1.5, 0.5, 1.5])
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images\{row['image']}" )
+        st.write(f"[source code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images\{row['image']}" )
+        st.write(f"[source code]({row['url']})")
+
 
