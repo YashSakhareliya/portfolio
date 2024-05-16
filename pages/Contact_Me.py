db1433db3c487f3963b5031b547c3d3fc2fa9da2
YashSakhareliya <sakhareliyayash@gmail.com>
2024-05-16 09:39:15 +0530

    email send complete , complete second web app

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,10 +1,16 @@
 import streamlit as st
+from send_mail import send_mail
 
 st.header("Contact Me")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your email address")
-    massage = st.text_area("Your message")
+    raw_message = st.text_area("Your message")
+
+    message = (f"Subject: New mail from {user_email} \n"
+               f"\nFrom: {user_email}  "
+               f"\nmessage: {raw_message}")
     button = st.form_submit_button("Submit")
     if button:
-        st.write(massage)
+        send_mail(message)
+        st.info("Email sent successfully")
