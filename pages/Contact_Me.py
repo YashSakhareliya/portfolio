9d6bf585e8090e594afd63e12d83f87208693d4a
YashSakhareliya <sakhareliyayash@gmail.com>
2024-05-16 08:41:09 +0530

    create contact me form

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,2 +1,10 @@
 import streamlit as st
 
+st.header("Contact Me")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Your email address")
+    massage = st.text_area("Your message")
+    button = st.form_submit_button("Submit")
+    if button:
+        st.write(massage)
